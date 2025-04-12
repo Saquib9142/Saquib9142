b1e088f479858e9cd17a48aec775fe153fd49541
Saquib9142 <saquibalam366@gmail.com>
2025-04-13 00:40:34 +0530

    upload.php

--- upload.php
+++ upload.php
@@ -1,50 +1,42 @@
 <?php
-if(isset($_POST["submit"])) {
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+if (isset($_POST['submit'])) {
+    $uploadDir = "uploads/";
+    $file = $_FILES['image'];
+    $fileName = basename($file['name']);
+    $fileTmp = $file['tmp_name'];
+    $fileSize = $file['size'];
+    $fileError = $file['error'];
 
-    // Check if file is an image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
+    $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
+    $ext = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+    if (!in_array($ext, $allowedTypes)) {
+        die("Only JPG, JPEG, PNG, and GIF files are allowed.");
     }
 
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
+    if ($fileError !== 0) {
+        die("There was an error uploading your file.");
     }
 
-    // Check file size (limit to 5MB)
-    if ($_FILES["fileToUpload"]["size"] > 5000000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
+    if ($fileSize > 5 * 1024 * 1024) {
+        die("File is too large. Maximum size is 5MB.");
     }
 
-    // Allow certain file formats (jpg, png, jpeg, gif)
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
+    // Create unique file name
+    $newName = uniqid("IMG_", true) . "." . $ext;
+
+    // Check if upload folder exists
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0755, true);
     }
 
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    // If everything is ok, try to upload file
+    $uploadPath = $uploadDir . $newName;
+
+    if (move_uploaded_file($fileTmp, $uploadPath)) {
+        echo "Image uploaded successfully: <br>";
+        echo "<img src='$uploadPath' alt='Uploaded Image' width='300'>";
     } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
+        echo "Failed to upload image.";
     }
 }
 ?>
